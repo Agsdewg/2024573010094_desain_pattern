1fa93f1e344ce8ee20e4897bccacebb65cfbbad2
agusdewangga <agusdewangga328@gmail.com>
2026-04-06 10:13:36 +0700

    modul 2

---
+++ src/Modul_3/bagian_4/overloading/latihan/BangunDatar.java
@@ -0,0 +1,12 @@
+package modul_3.bagian_4.overloading.latihan;
+
+class BangunDatar {
+    public void hitungLuas() {
+        System.out.println("Menghitung luas bangun datar...");
+    }
+}
+
+// Subclass Persegi
+
+
+
