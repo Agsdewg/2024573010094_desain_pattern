6959a5ddfe882d5fcf3fbdcd833738004fedd052
agusdewangga <agusdewangga328@gmail.com>
2026-04-06 10:19:03 +0700

    modul 2

---
+++ src/Modul_3/bagian_1/Main.java
@@ -0,0 +1,11 @@
+package praktikum_3.bagian_1;
+
+public class Main {
+    public static void main(String[] args) {
+        praktikum_3.bagian_1.Mahasiswa mhs1 = new praktikum_3.bagian_1.Mahasiswa();
+        mhs1.nama = "Budi";
+        mhs1.umur = 20;
+
+        mhs1.displayInfo();
+    }
+}
